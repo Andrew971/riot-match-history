63d32accc18bdd6bd1098f48b467460591afd72b
Andrew SORDIER <andrewsordier@MacBook-Pro.local>
2023-03-20 03:40:25 -0700

    doc(): updated readme with live demo url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,38 +5,36 @@ import { riotPlatform } from "lib/constant";
 import Main from "@components/main";
 import List from "@components/list";
 
-
 export default function Home() {
   const [_isPending, startTransition] = useTransition();
   const [searchTerm, setSearchTerm] = useState("");
   const [platform, setPlatform] = useState(riotPlatform.BR1.name);
   const [data, setData] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
 
   useUpdateEffect(() => {
-    if(!searchTerm || searchTerm === '') return
+    if (!searchTerm || searchTerm === "") return;
+    let isCurrent = true; // to avoid race condition
     fetch("/api/match-history", {
       method: "POST",
-      headers:{
-        "Content-Type":'application/json'
+      headers: {
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         searchTerm: searchTerm,
-        platform: platform
+        platform: platform,
       }),
     })
       .then((res) => res.json())
       .then((result) => {
+        if(!isCurrent) return
         setData(result.items);
-        if(result.error){
-          console.log(result)
-          setErrorMessage(result.error)
-        }
+        result.error && setErrorMessage(result.error);
       });
-      return () => {
-
-      }
-  }, [searchTerm,platform]);
+    return () => {
+      isCurrent = false;
+    };
+  }, [searchTerm, platform]);
 
   const onSubmitSearchTerm = useCallback((searchTerm: string) => {
     startTransition(() => {
@@ -48,12 +46,19 @@ export default function Home() {
       setPlatform(newPlatform);
     });
   }, []);
-  
+
   return (
     <Suspense fallback={<h1>🌀 Loading...</h1>}>
-      <AppBar onSearch={onSubmitSearchTerm}  onPlatformChange={onPlatformChange}/>
+      <AppBar
+        onSearch={onSubmitSearchTerm}
+        onPlatformChange={onPlatformChange}
+      />
       <Main>
-        <List items={data} searchTerm={searchTerm} errorMessage={errorMessage} />
+        <List
+          items={data}
+          searchTerm={searchTerm}
+          errorMessage={errorMessage}
+        />
       </Main>
     </Suspense>
   );
